b18e5a180662ab7d51dcbfec8d962b111af9f612
orochymaru25 <beatingo.rochy@gmail.com>
2023-01-18 23:07:59 +0800

    w3

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // Console.WriteLine("Hello Learning03 World!");
+        // Console.WriteLine();
+
+       
+        
+
     }
 }
